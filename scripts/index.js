85177675e4cc0188904cf974f915ceb2d2cb210c
gabrielsozinho <gabrielsozinho20@gmail.com>
2022-10-08 14:07:04 -0300

    feat: add navbar, light theme and about section

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+let theme_toggler = document.querySelector('#theme_toggler');
+var myCarousel = document.querySelector('#myCarousel')
+
+
+
+theme_toggler.addEventListener('click', function(){ 
+    document.body.classList.toggle('light_mode');
+
+});
+
+
+/*
+typescript
+nodejs
+reactjs
+nextjs
+postgresql
+figma*/
