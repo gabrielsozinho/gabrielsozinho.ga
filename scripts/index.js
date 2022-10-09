cfc4638d4f23b21d3dcada51b4eb8526b3561114
gabrielsozinho <gabrielsozinho20@gmail.com>
2022-10-09 16:31:52 -0300

    fix: remove cursor follower

--- scripts/index.js
+++ scripts/index.js
@@ -26,14 +26,6 @@ window.addEventListener("scroll", reveal);
 
 reveal();
 
-let circle = document.getElementById('circle');
-
-const onMouseMove = (e) =>{
-    circle.style.left = e.pageX + 'px';
-    circle.style.top = e.pageY + 'px';
-  }
-  
-document.addEventListener('mousemove', onMouseMove);
 /*
 typescript
 nodejs
